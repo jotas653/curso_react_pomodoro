c1eaf3bcee65746dcd3ecc3d8c207828dcdfe73a
Joao Barboza <joao.barboza@bit.com.br>
2026-02-09 14:52:31 -0300

    Novas mudanças

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import './styles/theme.css'
-import './styles/global.css'
-import Container from './components/Container'
-import { Heading } from './components/Heading'
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+import "./styles/global.css";
+import "./styles/theme.css";
 
-export function App () {
+export function App() {
   return (
-      <> 
+    <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
         <Heading>MENU</Heading>
       </Container>
-      </>
-  )
+    </>
+  );
 }
