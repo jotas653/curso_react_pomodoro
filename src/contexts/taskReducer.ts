d2d0870684c54bec6c24ad476d66fa08a8fc630a
Joao Barboza <joao.barboza@bit.com.br>
2026-03-23 13:54:24 -0300

    Novas alterações foram feitas

---
+++ src/contexts/taskReducer.ts
@@ -0,0 +1,17 @@
+import type { TaskStateModel } from "../models/TaskStateModel";
+import {
+  TaskActionTypes,
+  type TaskActionModel,
+} from "./TaskContext/taskActions";
+
+export function taskReducer(
+  state: TaskStateModel,
+  action: TaskActionModel,
+): TaskStateModel {
+  switch (action.type) {
+    case TaskActionTypes.START_TASK: {
+      return state;
+    }
+  }
+  return state;
+}
